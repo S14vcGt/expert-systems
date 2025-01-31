45281c608974cc7b6725f1238722aa8dff79acdc
Sebastián Mata <sebasmata41@gmail.com>
2025-01-31 00:45:00 +0000

    funciona

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, request
-from model.model import dumb_answer
 from flask_cors import CORS
+from model.model import user_friendly_answer
+model_input = []
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def get_user():
     query = request.args.get('user')
-    return dumb_answer(query)
+    if query == "no":
+        model_input.append(0)
+    else:
+        model_input.append(1)
+    if len(model_input) == 23:
+        model_answer= user_friendly_answer(model_answer)
+        model_input.clear() 
+        return model_answer
+    return f'ESTADO DE LA LISTA {model_input} '
 
 if __name__ == "__main__":
     app.run()
